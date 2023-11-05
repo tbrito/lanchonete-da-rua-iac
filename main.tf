7646caacb80fc94fa98b9fcce84180a4524c9f04
Tiago Brito <tiago.brito@wiz.co>
2023-11-05 17:43:51 -0300

    feat: provisionar database postgres

--- main.tf
+++ main.tf
@@ -67,3 +67,51 @@ resource "aws_api_gateway_stage" "lanchonetedarua" {
   rest_api_id   = aws_api_gateway_rest_api.lanchonetedarua.id
   stage_name    = "lanchonetedarua"
 }
+
+## vpc
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "lanchonetedarua"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "lanchonetedarua" {
+  name       = "lanchonetedarua"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "VPCsLanchonete"
+  }
+}
+
+#Instancia do banco
+resource "aws_db_instance" "lanchonetedarua" {
+  identifier             = "lanchonetedarua"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.1"
+  username               = "postgres"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.lanchonetedarua.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.lanchonetedarua.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
+
+resource "aws_db_parameter_group" "lanchonetedarua" {
+  name   = "lanchonetedarua"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
