6f3525d2d73890198e0450468e66a87f255c8ca5
Tiago Brito <tiago.brito@wiz.co>
2023-11-05 20:05:02 -0300

    feat: provision postgres 6

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_db_instance" "lanchonetedarua" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "13"
+  engine_version         = "14"
   username               = "postgres"
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.lanchonetedarua.name
@@ -134,7 +134,7 @@ resource "aws_db_instance" "lanchonetedarua" {
 
 resource "aws_db_parameter_group" "lanchonetedarua" {
   name   = "lanchonetedarua"
-  family = "postgres13"
+  family = "postgres14"
 
   parameter {
     name  = "log_connections"
