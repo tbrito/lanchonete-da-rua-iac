9290f3e8e64383da2249987b9987c9469aa3e96e
Tiago Brito <tiago.brito@wiz.co>
2023-11-05 14:43:36 -0300

    feat: api gateway

--- main.tf
+++ main.tf
@@ -20,6 +20,48 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_cognito_user_pool" "pool" {
-  name = "lanchoneteDaRua"
+resource "aws_api_gateway_rest_api" "lanchonetedarua" {
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = "lanchonetedarua"
+      version = "1.0"
+    }
+    paths = {
+      "/path1" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "GET"
+            payloadFormatVersion = "1.0"
+            type                 = "HTTP_PROXY"
+            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+          }
+        }
+      }
+    }
+  })
+
+  name = "lanchonetedarua"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "lanchonetedarua" {
+  rest_api_id = aws_api_gateway_rest_api.lanchonetedarua.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lanchonetedarua.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "lanchonetedarua" {
+  deployment_id = aws_api_gateway_deployment.lanchonetedarua.id
+  rest_api_id   = aws_api_gateway_rest_api.lanchonetedarua.id
+  stage_name    = "lanchonetedarua"
 }
