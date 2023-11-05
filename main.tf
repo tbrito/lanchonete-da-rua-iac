212ebe29005d4c6b72a137fc5dc2283ab8c800d1
Tiago Brito <tiago.brito@wiz.co>
2023-11-05 19:34:16 -0300

    feat: provision postgres 3

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_db_instance" "lanchonetedarua" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "14.1"
+  engine_version         = "16.0"
   username               = "postgres"
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.lanchonetedarua.name
