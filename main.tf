a833ae4f687a91c5bdc8c1091d05f1281bdc4c6d
Tiago Brito <tiago.brito@wiz.co>
2023-11-05 22:13:22 -0300

    feat: provision postgres 14

--- main.tf
+++ main.tf
@@ -203,7 +203,7 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 resource "aws_alb" "application_load_balancer" {
   name               = "load-balancer-dev" #load balancer name
   load_balancer_type = "application"
-  subnets = [ "${aws_db_subnet_group.lanchonetedarua2.id}" ]
+  subnets = [ for subnet in module.vpc.public_subnets : subnet.id ]
   security_groups = ["${aws_security_group.rds2.id}"]
 }
 
