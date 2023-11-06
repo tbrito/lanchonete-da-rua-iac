876f1213216c164c8f36007c2cfe65e0573a5435
Tiago Brito <e3205@itsector.pt>
2023-11-06 17:08:05 -0300

    feat add gateway and db 2

--- main.tf
+++ main.tf
@@ -117,14 +117,62 @@ resource "aws_security_group" "service_security_group" {
   }
 }
 
+# resource "aws_db_subnet_group" "lanchonetedarua2" {
+#   name       = "lanchonetedarua2"
+#   subnet_ids = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id]
+
+#   tags = {
+#     Name = "lanchonetedarua2 db sub"
+#   }
+# }
+
+### VPC-OLD
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "lanchonetedarua2"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
 resource "aws_db_subnet_group" "lanchonetedarua2" {
   name       = "lanchonetedarua2"
-  subnet_ids = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id]
+  subnet_ids = module.vpc.public_subnets
 
   tags = {
-    Name = "lanchonetedarua2 db sub"
+    Name = "VPCsLanchonete2"
   }
 }
+
+resource "aws_security_group" "rds2" {
+  name   = "lanchonetedarua2_rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lanchonetedarua2_rds"
+  }
+}
+####
 
 # Instancia do banco
 resource "aws_db_instance" "lanchonetedarua2" {
