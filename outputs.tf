8076bc1659c2e6007ce27dbbe59253bfea4a92d6
Tiago Brito <e3205@itsector.pt>
2023-11-06 17:18:30 -0300

    feat add gateway and db 5

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "rds2_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.lanchonetedarua2.address
+  value       = aws_db_instance.lanchonetedarua3.address
   sensitive   = true
 }
 
 output "rds2_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.lanchonetedarua2.port
+  value       = aws_db_instance.lanchonetedarua3.port
   sensitive   = true
 }
 
 output "rds2_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.lanchonetedarua2.username
+  value       = aws_db_instance.lanchonetedarua3.username
   sensitive   = true
 }
