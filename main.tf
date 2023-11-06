269bac6239d52c4a3d8db5aac77817f6b3a783c2
Tiago Brito <e3205@itsector.pt>
2023-11-06 19:50:25 -0300

    feat: ecs connect image to ecs

--- main.tf
+++ main.tf
@@ -201,52 +201,52 @@ resource "aws_ecs_cluster" "lanchonetedarua_cluster" {
   name = "lanchonetedarua-cluster" 
 }
 
-# resource "aws_ecs_task_definition" "app_task" {
-#   family                   = "app-first-task" # Name your task
-#   container_definitions    = <<DEFINITION
-#   [
-#     {
-#       "name": "app-first-task",
-#       "image": "${aws_ecr_repository.lanchonetedarua_ecr_repo.repository_url}",
-#       "essential": true,
-#       "portMappings": [
-#         {
-#           "containerPort": 5000,
-#           "hostPort": 5000
-#         }
-#       ],
-#       "memory": 512,
-#       "cpu": 256
-#     }
-#   ]
-#   DEFINITION
-#   requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
-#   network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-#   memory                   = 512         # Specify the memory the container requires
-#   cpu                      = 256         # Specify the CPU the container requires
-#   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
-# }
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "connect-image-lanchonete-to-ecs-task" # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "connect-image-lanchonete-to-ecs-task",
+      "image": "${aws_ecr_repository.lanchonetedarua_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
 
-# resource "aws_iam_role" "ecsTaskExecutionRole" {
-#   name               = "ecsTaskExecutionRole"
-#   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-# }
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
 
-# data "aws_iam_policy_document" "assume_role_policy" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["ecs-tasks.amazonaws.com"]
-#     }
-#   }
-# }
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
 
-# resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-#   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-# }
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 # # Load Balance
 # resource "aws_alb" "application_load_balancer" {
