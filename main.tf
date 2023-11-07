b11ada941fbfe15cff5e069d13f73b22b3f19bff
Tiago Brito <e3205@itsector.pt>
2023-11-07 19:08:01 -0300

    feat: lambda 4

--- main.tf
+++ main.tf
@@ -349,7 +349,7 @@ EOF
 
 resource "aws_lambda_permission" "url" {
   action        = "lambda:InvokeFunctionUrl"
-  function_name = aws_lambda_function.example.function_name
+  function_name = aws_lambda_function.generate_token_function.LanchoneteDaRua_GenerateToken_Function
   principal     = "arn:aws:iam::731628207007:role/authentication"
 
   source_account         = "731628207007"
@@ -365,8 +365,8 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 resource "aws_lambda_function" "generate_token_function" {
   #filename                       = "${path.module}/lambda_dist_pkg/generate-token.zip"
   source_code_hash               = null
-  function_name                  = "LanchoneteDaRua_Token_Lambda_Function"
-  role                           = aws_lambda_permission.url
+  function_name                  = "LanchoneteDaRua_GenerateToken_Function"
+  role                           = aws_iam_role.iam_for_lambda.arn
   handler                        = "lambda_function.lambda_handler"
   runtime                        = "python3.8"
   depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
