108f0e67bdc2ed38468cc9c16447f690f688f30f
Tiago Brito <e3205@itsector.pt>
2023-11-07 18:09:28 -0300

    feat: lambda 1

--- main.tf
+++ main.tf
@@ -304,3 +304,81 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 # output "app_url" {
 #   value = aws_alb.application_load_balancer.dns_name
 # }
+
+### Lambda ###
+resource "aws_iam_role" "lambda_role" {
+name   = "iamRoleLambdaFunctionRole"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Gerenciamento da politica das lambdas"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+## Anexar política do IAM à função do IAM
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+resource "null_resource" "install_python_dependencies" {
+  provisioner "local-exec" {
+    command = "bash ${path.module}/scripts/create_pkg.sh"
+
+    environment = {
+      source_code_path = "generate_token"
+      function_name = "LanchoneteDaRua_Token_Lambda_Function"
+      path_module = path.module
+      runtime = "python3.8"
+      path_cwd = path.cwd
+    }
+  }
+}
+
+data "archive_file" "zip_the_python_code" {
+  depends_on = ["null_resource.install_python_dependencies"]
+  type        = "zip"
+  source_dir  = "${path.module}/generate_token/"
+  output_path = "${path.module}/generate_token/generate-token.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/generate_token/hello-python.zip"
+function_name                  = "LanchoneteDaRua_Token_Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
