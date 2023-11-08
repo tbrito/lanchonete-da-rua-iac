fe00e7b391a50c9224e9653be0e0e711d8a83fec
lucasnishimoto <lucas.nishimoto@remessaonline.com.br>
2023-11-08 19:10:05 -0300

    fixed error vpc

--- main.tf
+++ main.tf
@@ -1244,11 +1244,12 @@ resource "aws_ecs_service" "app_service" {
   name            = "app-service"
   cluster         = aws_ecs_cluster.lanchonetedarua_cluster.id
   task_definition = aws_ecs_task_definition.app_task.arn
-  launch_type     = "FARGATE"  
+  launch_type     = "FARGATE"
   desired_count   = 1
+
   network_configuration {
-    subnets = [aws_subnet.example.id]
-    security_groups = [aws_security_group.example.id]
+    subnets         = module.vpc.public_subnets
+    security_groups = [aws_security_group.service_security_group.id]
   }
 
   load_balancer {
@@ -1257,6 +1258,58 @@ resource "aws_ecs_service" "app_service" {
     container_port   = 5000
   }
 }
+
+
+resource "aws_lb" "example" {
+  name               = "my-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "my-lb"
+  }
+}
+
+resource "aws_lb_target_group" "example" {
+  name     = "my-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "my-tg"
+  }
+}
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
+
+
+
 
 # Output the ECS Cluster Name
 output "ecs_cluster_name" {
