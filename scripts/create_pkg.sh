1daef158744d90ad94764c44af599dba1a032ebb
Janduy Monroe <janduymonroe@gmail.com>
2023-11-08 13:14:17 -0300

    fix: remove changes

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -26,22 +26,13 @@ fi
 deactivate
 
 # Create deployment package...
-# echo "Creating deployment package..."
-# cd env_$function_name/lib/$runtime/site-packages/
-# cp -r . $path_cwd/$dir_name
-# cp -r $path_cwd/$function_name/* $path_cwd/$
-# cd $path_cwd/$dir_name && zip -r ../$function_name.zip .
-
-# Copy function code to the package directory...
 echo "Creating deployment package..."
+cd env_$function_name/lib/$runtime/site-packages/
+cp -r . $path_cwd/$dir_name
 cp -r $path_cwd/$function_name/* $path_cwd/$dir_name
-
-# Create a ZIP file of the package directory...
-cd $path_cwd/$dir_name
-zip -r ./$function_name.zip .
 
 # Removing virtual environment folder...
 echo "Removing virtual environment folder..."
-# rm -rf $path_cwd/env_$function_name
+rm -rf $path_cwd/env_$function_name
 
 echo "Finished script execution!"
