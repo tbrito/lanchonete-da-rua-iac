10be9c6ad95559802bdbd6377a9a11dda86779cf
lucasnishimoto <lucas.nishimoto@remessaonline.com.br>
2023-11-08 19:25:29 -0300

    fixed loadbalance

--- main.tf
+++ main.tf
@@ -1279,6 +1279,7 @@ resource "aws_lb_target_group" "example" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
+  target_type = "ip"  # This is the required change
 
   health_check {
     enabled             = true
@@ -1289,13 +1290,14 @@ resource "aws_lb_target_group" "example" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 5
-    matcher             = "200"
+    matcher             = "200-499"
   }
 
   tags = {
-    Name = "my-tg"
+    Name = "aws-load"
   }
 }
+
 
 resource "aws_lb_listener" "example" {
   load_balancer_arn = aws_lb.example.arn
