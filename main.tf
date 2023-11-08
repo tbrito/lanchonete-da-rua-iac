3f3c473e5f06e09d850a4cd85e932e0964491414
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-11-07 21:29:22 -0300

    lambda 25

--- main.tf
+++ main.tf
@@ -390,7 +390,8 @@ resource "aws_lambda_function" "generate_token_function" {
   function_name                  = "lanchonete_generate_token"
   role                           = aws_iam_role.lambda_role.arn
   handler                        = "lambda_function.lambda_handler"
-  runtime                        = "python3.11"
+  runtime                        = "python3.9"
   publish                        = false
-  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, null_resource.install_python_dependencies]
+  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]  
+  # depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, null_resource.install_python_dependencies]
 }
