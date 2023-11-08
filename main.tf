a88eafe0c12b43aa1a98780254488ce7cf3d8351
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-11-08 09:00:58 -0300

    lambda 1

--- main.tf
+++ main.tf
@@ -342,49 +342,48 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
 EOF
 }
 
-# resource "aws_lambda_permission" "allow_generate_token" {
-#    statement_id  = "AllowMyroleAuthentication"
-#    action        = "lambda:InvokeFunction"
-#    function_name = "lanchonete_generate_token"
-#    principal     = "events.amazonaws.com"
-#    source_arn    = "arn:aws:iam::990304834518:role/authentication"
-#    source_account         = "990304834518"
-#    function_url_auth_type = "AWS_IAM"
-# }
+resource "aws_lambda_permission" "allow_generate_token" {
+    statement_id  = "AllowMyroleAuthentication"
+    action        = "lambda:InvokeFunction"
+    function_name = "lanchonete_generate_token"
+    principal     = "events.amazonaws.com"
+    source_arn    = "arn:aws:iam::990304834518:role/authentication"
+    source_account         = "990304834518"
+    function_url_auth_type = "AWS_IAM"
+}
 
-# ## Anexar política do IAM à função do IAM
-# resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-#  role        = aws_iam_role.lambda_role.name
-#  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
-# }
+## Anexar política do IAM à função do IAM
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
 
-# resource "null_resource" "install_python_dependencies" {
-#   provisioner "local-exec" {
-#     command = "bash ${path.module}/scripts/create_pkg.sh"
+resource "null_resource" "install_python_dependencies" {
+  provisioner "local-exec" {
+    command = "bash ${path.module}/scripts/create_pkg.sh"
 
-#     environment = {
-#       source_code_path = "generate_token"
-#       function_name    = "lanchonete_generate_token"
-#       path_module      = path.module
-#       runtime          = "python3.8"
-#       path_cwd         = path.cwd
-#     }
-#   }
-# }
+    environment = {
+      source_code_path = "generate_token"
+      function_name    = "lanchonete_generate_token"
+      path_module      = path.module
+      runtime          = "python3.8"
+      path_cwd         = path.cwd
+    }
+  }
+}
 
-# data "archive_file" "zip_the_python_code" {
-#  depends_on  = ["null_resource.install_python_dependencies"]
-#  type        = "zip"
-#  source_dir  = "${path.module}/generate_token/"
-#  output_path = "${path.module}/lambda_dist_pkg/generate-token.zip"
-# }
+ data "archive_file" "zip_the_python_code" {
+  depends_on  = ["null_resource.install_python_dependencies"]
+  type        = "zip"
+  source_dir  = "${path.module}/generate_token/"
+  output_path = "${path.module}/lambda_dist_pkg/generate-token.zip"
+ }
 
-# resource "aws_lambda_function" "generate_token_function" {
-#  # filename                       = "${path.module}/lambda_dist_pkg/generate-token.zip"
-#   function_name                  = "lanchonete_generate_token"
-#   role                           = aws_iam_role.lambda_role.arn
-#  # handler                        = "lambda_function.lambda_handler"
-#  # runtime                        = "python3.9"
-#  # publish                        = false
-#  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, null_resource.install_python_dependencies]
-# }
+ resource "aws_lambda_function" "generate_token_function" {
+  filename                       = "${path.module}/lambda_dist_pkg/generate-token.zip"
+  function_name                  = "lanchonete_generate_token"
+  role                           = aws_iam_role.lambda_role.arn
+  handler                        = "lambda_function.lambda_handler"
+  runtime                        = "python3.9"
+  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, null_resource.install_python_dependencies]
+ }
