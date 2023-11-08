b2005190273242f8cf3745a18ccfe9ad0b441a33
Janduy Monroe <janduymonroe@gmail.com>
2023-11-08 10:52:33 -0300

    fix: fixed name

--- main.tf
+++ main.tf
@@ -406,7 +406,7 @@ resource "null_resource" "install_python_dependencies" {
   }
 }
 
- data "archive_file" "zip_the_python_code" {
+ data "archive_file" "zip_the_python_code_2" {
   depends_on  = ["null_resource.create_package"]
   type        = "zip"
   source_dir  = "${path.module}/check_token/"
