29360e4cf74dbcfcf695255e852e5db4aa4fa165
Tiago Brito <e3205@itsector.pt>
2023-12-20 13:56:04 -0300

    feat: ecr and gateway pedidos produtos

--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
-variable "db_password" {
-  description = "RDS root user password"
+variable "ecr_name" {
+  description = "The name of the ECR registry"
+  type        = any
+  default     = null
+}
+
+variable "image_mutability" {
+  description = "Provide image mutability"
   type        = string
-  sensitive   = true
+  default     = "IMMUTABLE"
+}
+
+
+variable "encrypt_type" {
+  description = "Provide type of encryption here"
+  type        = string
+  default     = "KMS"
+}
+
+variable "tags" {
+  description = "The key-value maps for tagging"
+  type        = map(string)
+  default     = {}
 }
