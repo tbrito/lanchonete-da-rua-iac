5de29bb355a8744353b175788f137b4f3251f209
Tiago Brito <e3205@itsector.pt>
2024-03-18 11:50:31 -0300

    chore: infraestrutura fase 5

--- main.tf
+++ main.tf
@@ -7,32 +7,32 @@ terraform {
   }
   required_version = ">= 1.1.0"
 
-  cloud {
-    organization = "grupo23postech"
+  # cloud {
+  #   organization = "grupo23postechfiapi"
 
-    workspaces {
-      name = "lanchonete-da-rua-iac"
-    }
-  }
+  #   workspaces {
+  #     name = "lanchonete-da-rua-iac"
+  #   }
+  # }
 }
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "ASIAWZ6JU6KTOXQDAYKD"
-  secret_key = "sKI3gIWKUwW+rhPuS5eCx77zbcSkBrhXlazktBG1"
-  token = "FwoGZXIvYXdzEBYaDHKW3fppRB4R0DBTKiLFAfrgqFScopJRgMEaqqgN2ZLFeaira8w5KgxgthEzHvDoVzGsoYKS9VMX/hX0xYUGpI7m9VmOC8gqofuHG1wE7eNEssaq8GuB58FCCoEJkN6TLYBSuTsnYhNofhwgddUIXpcLvG9Oo1K9jReyetWqKbJVpiMdEtAEEgXJ2kQ699kW46NuYCuwpSHeKZTk5bfgWb0cJF/s4ej3WrRw/kSDEMmwtzCoy36OaYnmOJw5wX7sTuT2Il57UPoqoaGUQHoQVsxLi5K3KKHHm60GMi09YeuOCnoORPLpGfm22r9WsDcTLqnWf0mta7+z9eX0hXlJLzG7a6YtPzwslOI="
+  access_key = "AKIAU6GD3QJR2EMRVYOR"
+  secret_key = "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"
+  # token = "FwoGZXIvYXdzEBYaDHKW3fppRB4R0DBTKiLFAfrgqFScopJRgMEaqqgN2ZLFeaira8w5KgxgthEzHvDoVzGsoYKS9VMX/hX0xYUGpI7m9VmOC8gqofuHG1wE7eNEssaq8GuB58FCCoEJkN6TLYBSuTsnYhNofhwgddUIXpcLvG9Oo1K9jReyetWqKbJVpiMdEtAEEgXJ2kQ699kW46NuYCuwpSHeKZTk5bfgWb0cJF/s4ej3WrRw/kSDEMmwtzCoy36OaYnmOJw5wX7sTuT2Il57UPoqoaGUQHoQVsxLi5K3KKHHm60GMi09YeuOCnoORPLpGfm22r9WsDcTLqnWf0mta7+z9eX0hXlJLzG7a6YtPzwslOI="
 }
 
 data "aws_subnet" "subnet1a" {
-  id = "subnet-04de00c5de4379413"
+  id = "subnet-03df6cdb0ce9d859b"
 }
 
 data "aws_subnet" "subnet1b" {
-  id = "subnet-08b81483df03e0a52"
+  id = "subnet-0e276900afb704937"
 }
 
 data "aws_vpc" "default" {
-  id = "vpc-004a9706c20b9ae57"
+  id = "vpc-04adaa77e36d1d98c"
 }
 
 data "aws_caller_identity" "current" {}
